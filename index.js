737b8925bb0796be0d3ee4ca1e048a0435859312
Eric Krukowski <erickruk@amazon.com>
2023-03-20 18:56:11 -0400

    completed POST send, basework for adding to DOM and error catching laid out

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const newUser = {
+        name: userName,
+        email: userEmail
+    }
+    const PostData = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(newUser)
+    }
+    return fetch("http://localhost:3000/users", PostData)
+    .then(resp =>resp.json())
+    .then(user =>{
+        console.log(user)
+    })
+    .catch(e => console.log(`Recieved error: ${e}`))
+}
